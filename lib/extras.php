b78680c83ad9d8819e7005ce07482980aef2aebe
Mike iLL Kilmer <mike@mzoo.org>
2018-10-03 11:01:56 -0500

    Display large front page buttons with a shortcode.

--- lib/extras.php
+++ lib/extras.php
@@ -107,7 +107,7 @@ function mz_noahkenin_add_frontpage_gallery() {
     'post_type' => 'portfolio'
   );
   //var_dump(get_post_type_archive_link( 'portfolio' ));
-  // var_dump( get_intermediate_image_sizes() );
+  //var_dump( get_intermediate_image_sizes() );
   $gallery_results = new \WP_Query($args);
   $result = '';
   $count = 0;
@@ -151,3 +151,38 @@ function wp_mz_67472455(){
 }
 add_action( 'wp_print_styles', __NAMESPACE__ . '\\wp_mz_67472455', 100 );
 
+function mz_noahkenin_large_frontpage_buttons() {
+return <<<EOL
+<div class="gallery-archive-container">
+	<div class="hp-gallery hp-archive-gallery">
+		<div class="hp-archive-gallery__content">
+
+		<svg class="hp-gallery__icon hp-archive-gallery__icon" id=Capa_1 style="enable-background:new 0 0 20.954 20.954"version=1.1 viewBox="0 0 20.954 20.954"x=0px xml:space=preserve xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink y=0px><g><g><g><path d="M20.851,7.169l-7.705-3.681c-0.09-0.042-0.197-0.004-0.24,0.084l-1.677,3.51h0.913l1.232-2.579
+				l6.679,3.191l-2.088,4.372l-1.177-0.561v2.311l0.723,0.346c0.091,0.043,0.197,0.004,0.241-0.084l3.185-6.668
+				C20.98,7.32,20.941,7.212,20.851,7.169z"style=fill:#030104 /></g><g><path d="M0.103,7.126l7.705-3.68c0.09-0.043,0.197-0.005,0.24,0.083l1.678,3.51H8.812L7.58,4.46
+				L0.901,7.651l2.089,4.372l1.176-0.561v2.311l-0.723,0.346c-0.09,0.045-0.197,0.004-0.24-0.084L0.018,7.366
+				C-0.025,7.277,0.013,7.17,0.103,7.126z"style=fill:#030104 /></g><g><path d="M16.06,7.684H5.216c-0.126,0-0.229,0.102-0.229,0.228v9.385c0,0.125,0.103,0.229,0.229,0.229
+				h10.845c0.127,0,0.229-0.104,0.229-0.229V7.912C16.29,7.786,16.187,7.684,16.06,7.684z M15.434,14.877h-0.688
+				c-0.4-1.025-0.893-2.463-1.641-2.271c-0.876,0.223-1.315,2.271-1.315,2.271s-0.446-2.311-1.683-3.549
+				c-1.238-1.237-2.437,3.549-2.437,3.549H6.033V8.723h9.401V14.877z"style=fill:#030104 /></g><g><circle cx=7.55 cy=10.042 r=0.767 style=fill:#030104 /></g><g><path d="M12.697,10.584c0.172,0,0.332-0.019,0.476-0.048c0.167,0.071,0.372,0.115,0.592,0.115
+				c0.564,0,1.023-0.276,1.023-0.616c0-0.341-0.459-0.618-1.023-0.618c-0.212,0-0.409,0.04-0.572,0.106
+				c-0.067-0.065-0.157-0.106-0.257-0.106h-0.149c-0.189,0-0.339,0.144-0.361,0.328c-0.458,0.048-0.798,0.213-0.798,0.413
+				C11.627,10.393,12.106,10.584,12.697,10.584z"style=fill:#030104 /></g></g></g></svg>
+
+				<br><h2 class="hp-archive-gallery__title">Gallery</h2>
+		</div>
+	</div>
+	<div class="hp-archive hp-archive-gallery">
+		<div class="hp-archive-gallery__content">
+
+			<svg class="hp-archive__icon hp-archive-gallery__icon" style="enable-background:new 0 0 511 511"version=1.1 viewBox="0 0 511 511"x=0px xml:space=preserve xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink y=0px><g><path d="M511,71.553H226.606L179.5,24.447l-47.106,47.106H0v383h16v32h47v-32h385v32h47v-32h16V71.553z M245.394,111.553h-43.787
+			l7.197-7.197L198.197,93.75l-17.803,17.803h-18.787l27.197-27.197L178.197,73.75l-37.803,37.803h-26.787L179.5,45.66
+			L245.394,111.553z M456,126.553v113H55v-113H456z M48,471.553H31v-17h17V471.553z M480,471.553h-17v-17h17V471.553z M496,439.553
+			h-1h-47H63H16h-1v-353h102.394l-25,25H40v143h431v-143H266.606l-25-25H496V439.553z"/><path d="M40,414.553h431v-143H40V414.553z M55,286.553h401v113H55V286.553z"/><path d="M327,159.553H184v47h143V159.553z M312,191.553H199v-17h113V191.553z"/><path d="M184,366.553h143v-47H184V366.553z M199,334.553h113v17H199V334.553z"/></g></svg>
+			<br><h2 class="hp-archive-gallery__title">Archive</h2>
+		</div>
+	</div>
+</div>
+EOL;
+}
+add_shortcode('large_frontpage_buttons', __NAMESPACE__ . '\\mz_noahkenin_large_frontpage_buttons');
