d4f88bac7fae20bb294a1f52b96e64642e9bd21a
Mike iLL Kilmer <mike@mzoo.org>
2018-10-04 08:19:03 -0500

    Reinstate header menu.

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,26 @@
 <header class="banner">
   <div class="container">
-    <div class="navbar navbar-expand-md navbar-light bg-faded">
-      <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    </div>
+    <nav class="navbar navbar-expand-md navbar-light bg-faded">
+       <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <?php
+       if (has_nav_menu('primary_navigation')) :
+         wp_nav_menu([
+           'theme_location'  => 'primary_navigation',
+           'container'       => 'div',
+           'container_id'    => 'bs4navbar',
+           'container_class' => 'collapse navbar-collapse',
+           'menu_id'         => false,
+           'menu_class'      => 'navbar-nav mr-auto',
+           'depth'           => 2,
+           'fallback_cb'     => 'bs4navwalker::fallback',
+           'walker'          => new bs4navwalker()
+         ]);
+      endif;
+       ?>
+
+    </nav>
   </div>
 </header>
